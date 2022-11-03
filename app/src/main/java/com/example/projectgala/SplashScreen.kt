4c7b7a21d266475f81db4375b35db7ba49399096
galachka <esbolov.galymzhan2003@gmail.com>
2022-11-04 00:13:52 +0600

    add fragment navigation 1

--- app/src/main/java/com/example/projectgala/SplashScreen.kt
+++ app/src/main/java/com/example/projectgala/SplashScreen.kt
@@ -10,6 +10,7 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
         } ,3000)
