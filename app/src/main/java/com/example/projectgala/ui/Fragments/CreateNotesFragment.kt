91eb2a3a3d14aecb5c1a210a6e5b59f24d15baa2
galachka <esbolov.galymzhan2003@gmail.com>
2022-11-04 01:12:32 +0600

    creat fragment add notes part 1

--- app/src/main/java/com/example/projectgala/ui/Fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/example/projectgala/ui/Fragments/CreateNotesFragment.kt
@@ -6,14 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.projectgala.R
+import com.example.projectgala.databinding.FragmentCreateNotesBinding
 
 
 class CreateNotesFragment : Fragment() {
+    lateinit var binding: FragmentCreateNotesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_notes, container, false)
+        binding = FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/projectgala/ui/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/projectgala/ui/Fragments/HomeFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.projectgala.R
+import com.example.projectgala.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        // с главного окна в дополнительное
+        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        binding.btnAddNotes.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_createNotesFragment)
+        }
+        return binding.root
     }
 }
