4d57d43a13cfa9d59b4aa6d2c54ed6bfe52aa986
galachka <esbolov.galymzhan2003@gmail.com>
2022-11-07 18:16:01 +0600

    create entity

---
+++ app/src/main/java/com/example/projectgala/Model/Notes.kt
@@ -0,0 +1,14 @@
+package com.example.projectgala.Model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Notes")
+class Notes (  @PrimaryKey(autoGenerate = true)
+               var id:Int? = null,
+               var title:String,
+               var subtitle:String,
+               var notes:String,
+               var date:String,
+               var priority:String
+)
