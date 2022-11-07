f24a4306206e8a3d313a9e906fb85b50e07b71cd
galachka <esbolov.galymzhan2003@gmail.com>
2022-11-07 19:10:09 +0600

    create database 2

--- app/src/main/java/com/example/projectgala/Database/NotesDatabase.kt
+++ app/src/main/java/com/example/projectgala/Database/NotesDatabase.kt
@@ -1,4 +1,31 @@
 package com.example.projectgala.Database
 
-class NotesDatabase {
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.projectgala.Dao.NotesDao
+
+abstract class NotesDatabase : RoomDatabase() {
+    abstract fun myNotesDao(): NotesDao
+
+    companion object{
+        @Volatile
+        var INSTANCE: NotesDatabase? = null
+
+        fun getDatabaseInstance(context: Context): NotesDatabase{
+
+            val tempInstance = INSTANCE
+            if(tempInstance!= null){
+                return tempInstance
+            }
+            synchronized(this)
+            {
+                val roomDatabaseInstance =
+                    Room.databaseBuilder(context,NotesDatabase::class.java,"Notes").build()
+                INSTANCE = roomDatabaseInstance
+                return roomDatabaseInstance
+            }
+        }
+
+    }
 }
