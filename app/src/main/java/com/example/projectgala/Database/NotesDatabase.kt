008c5435d420da0d05c833cbea973c429dbb8d8b
galachka <esbolov.galymzhan2003@gmail.com>
2022-11-07 21:31:46 +0600

    create notes part 3

--- app/src/main/java/com/example/projectgala/Database/NotesDatabase.kt
+++ app/src/main/java/com/example/projectgala/Database/NotesDatabase.kt
@@ -1,10 +1,13 @@
 package com.example.projectgala.Database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.projectgala.Dao.NotesDao
+import com.example.projectgala.Model.Notes
 
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class NotesDatabase : RoomDatabase() {
     abstract fun myNotesDao(): NotesDao
 
@@ -21,7 +24,10 @@ abstract class NotesDatabase : RoomDatabase() {
             synchronized(this)
             {
                 val roomDatabaseInstance =
-                    Room.databaseBuilder(context,NotesDatabase::class.java,"Notes").build()
+                    Room.databaseBuilder(
+                        context,NotesDatabase::class.java,
+                        "Notes"
+                    ).allowMainThreadQueries().build()
                 INSTANCE = roomDatabaseInstance
                 return roomDatabaseInstance
             }
--- app/src/main/java/com/example/projectgala/ui/Fragments/CreateNotesFragment.kt
+++ app/src/main/java/com/example/projectgala/ui/Fragments/CreateNotesFragment.kt
@@ -7,30 +7,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.projectgala.Model.Notes
 import com.example.projectgala.R
+import com.example.projectgala.ViewModel.NotesViewModel
 import com.example.projectgala.databinding.FragmentCreateNotesBinding
 import java.util.Date
 
 
 class CreateNotesFragment : Fragment() {
     lateinit var binding: FragmentCreateNotesBinding
+    val viewModel : NotesViewModel by viewModels()
+    var priority: String = "1"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCreateNotesBinding.inflate(layoutInflater,container,false)
 
+        binding.pGreen.setImageResource(R.drawable.ic_baseline_check_24)
+
         binding.pGreen.setOnClickListener{
+            priority = "1"
             binding.pGreen.setImageResource(R.drawable.ic_baseline_check_24)
             binding.pRed.setImageResource(0)
             binding.pYellow.setImageResource(0)
         }
         binding.pYellow.setOnClickListener{
+            priority = "2"
             binding.pYellow.setImageResource(R.drawable.ic_baseline_check_24)
             binding.pRed.setImageResource(0)
             binding.pGreen.setImageResource(0)
         }
         binding.pRed.setOnClickListener{
+            priority = "3"
             binding.pRed.setImageResource(R.drawable.ic_baseline_check_24)
             binding.pGreen.setImageResource(0)
             binding.pYellow.setImageResource(0)
@@ -43,12 +55,21 @@ class CreateNotesFragment : Fragment() {
     }
 
     private fun createNotes(it: View?) {
-        val title = binding.editTitle.text
-        val subTitle = binding.editSubtitle.text
-        val notes = binding.editNotes.text
+        val title = binding.editTitle.text.toString()
+        val subTitle = binding.editSubtitle.text.toString()
+        val notes = binding.editNotes.text.toString()
         val d = Date()
-        val s: CharSequence = DateFormat.format("MMMM d, yyyy", d.getTime())
+        val notesDate: CharSequence = DateFormat.format("MMMM d, yyyy", d.getTime())
+        val data = Notes(null,
+            title = title,
+            subtitle = subTitle,
+            notes = notes,
+            date = notesDate.toString(),
+            priority
 
+        )
+        viewModel.addNotes(data)
 
+        Toast.makeText(requireContext(), "Notes Greated Successfully", Toast.LENGTH_SHORT).show()
     }
 }
